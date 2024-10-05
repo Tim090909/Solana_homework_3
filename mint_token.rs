c027063c33af3e5b80c151256fb519919031cf62
Kyrylog <33864106+Kyrylog@users.noreply.github.com>
2024-10-05 13:22:33 +0300

    Update mint_token.rs

--- mint_token.rs
+++ mint_token.rs
@@ -1,4 +1,3 @@
-use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -13,12 +12,6 @@ use spl_token::{
     state::Mint,
 };
 
-/// Define the type of state stored in accounts
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct GreetingAccount {
-    /// number of greetings
-    pub counter: u32,
-}
 
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
